6966af97d660bc9652e5fc6c31c9c66ba7a2b13f
Eduard Malkhasyan <eduardmalkhasyan@gmail.com>
2025-04-03 00:09:34 +0400

    start

---
+++ Appodeal-iOS/Appodeal_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Appodeal_iOSApp.swift
+//  Appodeal-iOS
+//
+//  Created by Eduard Malkhasyan on 03.04.25.
+//
+
+import SwiftUI
+
+@main
+struct Appodeal_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Appodeal-iOSTests/Appodeal_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Appodeal_iOSTests.swift
+//  Appodeal-iOSTests
+//
+//  Created by Eduard Malkhasyan on 03.04.25.
+//
+
+import Testing
+@testable import Appodeal_iOS
+
+struct Appodeal_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
